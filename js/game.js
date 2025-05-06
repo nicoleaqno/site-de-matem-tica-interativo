750a131627a1284ccab8521045fdfabd9ab0fd41
nicoleaqno <nicole.aquino.rosa@escola.pr.gov.br>
2025-05-06 07:29:45 -0300

    Update game.js
    Signed-off-by: nicoleaqno <nicole.aquino.rosa@escola.pr.gov.br>

--- js/game.js
+++ js/game.js
@@ -47,7 +47,7 @@ function calcularResposta(n1, n2, op) {
     switch (op) {
         case '+': return n1 + n2;
         case '-': return n1 - n2;
-        case '*': return n1 * n2
+        case '*': return n1 * n2;
  }
 }
 
